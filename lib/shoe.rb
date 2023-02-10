26e7888959dd55bb6a665873df585f36302f57e7
dennis mwaniki <dennomaz@gmail.com>
2023-02-10 13:16:30 +0300

    donr

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color ,:size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+        @color = "red"
+        @size = 9.5
+        @material = "suede"
+        @condition = "tattered"
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
